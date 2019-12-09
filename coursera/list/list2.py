08ef2d75816b67db911ae9d32efc5c202de8fc0b
rashmicode11 <58022492+rashmicode11@users.noreply.github.com>
2019-12-09 22:45:44 +0530

    Write a function called chop that takes a list and modiﬁes it, removing the ﬁrst and last elements, and returns None. Then write a function called middle that takes a list and returns a new list that contains all but the ﬁrst and last elements.

---
+++ coursera/list/list2.py
@@ -0,0 +1,11 @@
+def chop():
+    list1=[1,2,3,4,5,6,7,8]
+    list1.pop(0)
+    list1.pop(len(list1)-1)
+    return None
+def middle():
+    list2=[1,2,3,4,5,6,7,8]
+    list2.pop(0)
+    list2.pop(len(list2)-1)
+    return list2
+
